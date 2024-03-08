afd61705700a5a1074f1feef57204e21616e03cc
ElenaRios <45754594+ElenaRios@users.noreply.github.com>
2024-03-08 13:41:06 +0100

    Update graphicage_streamlit_2902.py

--- graphicage_streamlit_2902.py
+++ graphicage_streamlit_2902.py
@@ -1,6 +1,3 @@
-
-## LAST UPDATE 05/03
-
 import streamlit as st
 from datetime import datetime, timedelta, time
 import matplotlib.pyplot as plt
@@ -8,10 +5,13 @@ import re
 import io
 import base64
 import array
-import random
+from PIL import Image
+from matplotlib.patches import Rectangle
 
 
-# LAST UPDATE 05/03
+
+
+
 
 
 def validate_duration_format(duration_str):
@@ -33,7 +33,7 @@ def get_days_of_service(bus_number, direction):
                     "Sunday": 7
                 }
 
-                jours_selectionnes = st.multiselect(f"Quel(s) jour(s) le bus {bus_number} roule de {direction} ?", list(jours_map.keys()))
+                jours_selectionnes = st.multiselect(f"Circulation day(s) of {bus_number} from {direction.replace('>', 'to')} ?", list(jours_map.keys()))
                 days = []
                 
                 #if "Everyday" in jours_selectionnes:
@@ -65,7 +65,7 @@ def plot_arrow(ax, start, end, color):
 def number_buses():
     while True:
         try:
-            num_buses = st.slider("Number of buses 🚌:", min_value=1, max_value=10, value=1, step=1)
+            num_buses = st.sidebar.slider("Number of buses 🚌:", min_value=1, max_value=10, value=1, step=1)
 
             #st.sidebar.write("Number of buses 🚌 :", num_buses)
             print("\n")
@@ -76,7 +76,7 @@ def number_buses():
 def graphicage_hlp():
     while True:
         try:
-            choice = st.radio("How many cities ? 🚏", options=[2, 3], format_func=lambda x: f"{x}")
+            choice = st.sidebar.radio("How many stops ? 🚏", options=[2, 3], format_func=lambda x: f"{x}")
             #st.write("Choix sélectionné :", choice)
             if choice == 2:
                 num_villes = 2
@@ -91,6 +91,22 @@ def graphicage_hlp():
             print("\n")
 
 widget_counter = 0
+
+def additional_info_optional():
+    bus_partner = st.sidebar.text_input(f"🤝 Bus Partner")
+    depot = st.sidebar.text_input(f"🏠 Deposit")
+    min_date = datetime.strptime('01/01/2022', '%d/%m/%Y')
+    odc = st.sidebar.date_input("🗓️ First Circulation Date", min_value=  min_date)
+    #other = st.sidebar.text_input(f"📝 Comments")
+    additional_info = {
+        '🤝 Bus Partner': bus_partner,
+        '🏠 Deposit': depot,
+        '🗓️ Start date': odc
+    }
+    return bus_partner, depot, odc, additional_info
+
+
+
 
 def get_unique_key(name):
     return f"{name}_{hash(name)}"
@@ -108,10 +124,98 @@ def get_image_download_link(buffer, filename):
     href = f'<a href="data:image/png;base64,{buffer_str}" download="{filename}">Download Image</a>'
     return href
 
-            
+def table(horaires_trajets, num_buses, days_of_services, colors, bbox):
+    table_cell = [['' for _ in range(len(horaires_trajets) + 1)] for _ in range(num_buses + 1)]
+    table_colors = [['black' for _ in range(len(horaires_trajets) + 1)] for _ in range(num_buses + 1)]
+    
+    # Populate headers
+    headers = list(horaires_trajets.keys())
+    table_cell[0][1:] = headers
+
+    # Populate table data and colors
+    for i in range(num_buses):
+        table_cell[i + 1][0] = f'Bus {i + 1}'
+        for j, key in enumerate(headers):
+            days_services = ''
+            for d in days_of_services.get((i + 1, key), ''):
+                days_services = days_services + str(d) + ' '
+            table_cell[i + 1][j + 1] = days_services
+            table_colors[i + 1][j + 1] = colors[i]
+    
+    # Create table
+    table = plt.table(cellText=table_cell,
+                      cellLoc='center',
+                      loc='lower right',
+                      bbox=bbox)
+    
+    # Set cell colors
+    for i in range(len(table_cell)):
+        for j in range(len(table_cell[i])):
+            cell = table[i, j]
+            cell_text = table_cell[i][j]
+            cell_color = table_colors[i][j]
+            cell.get_text().set_color(cell_color)
+    
+    
+    # Set border between first and second row
+    for i in range(len(table_cell)):
+        for j in range(len(table_cell[i])):
+            # Outline the entire first column
+            cell = table[i, j]
+            cell.set_edgecolor('lightgrey')
+            cell.set_linewidth(1)
+    
+    # Adjust font size
+    table.auto_set_font_size(False)  # Disable font size auto-scaling
+    table.set_fontsize(13)  # Set the font size manually
+    
+    # Show plot
+    plt.show() 
+
+def table_other_info(additional_info, bbox):
+    # Initialize table data and colors
+    num_items = len(additional_info)
+    table_cell = [['' for _ in range(2 * num_items)]]
+
+    # Populate table data and colors
+    for i, (key, value) in enumerate(additional_info.items()):
+        if value != '':
+            table_cell[0][2*i] = f"{key[1:]} : {value}"
+
+    # Create table
+    table = plt.table(cellText=table_cell,
+                      cellLoc='left',
+                      loc='upper right',
+                      bbox=bbox)
+    for i in range(len(table_cell)):
+        for j in range(len(table_cell[i])):
+            cell = table[i, j]
+            cell.get_text().set_color('#5f003c')
+
+    
+
+    # Set border between cells
+    for cell in table._cells:
+        table._cells[cell].set_edgecolor('lightgrey')
+        table._cells[cell].set_linewidth(1)
+
+    # Adjust font size
+    table.auto_set_font_size(False)  # Disable font size auto-scaling
+    table.set_fontsize(14)  # Set the font size manually
+
+    # Hide cell edges
+    for cell in table._cells:
+        table._cells[cell].set_edgecolor('none')
+
+    # Show plot
+    plt.show()
+
+        
+
 def horaires():
     num_villes = graphicage_hlp()
     num_buses = number_buses()
+    bus_partner, depot, odc, additional_info = additional_info_optional()
 
     villes = {}
     horaires_trajets = {}
@@ -126,8 +230,8 @@ def horaires():
             while True:
                 try:
                     key_1 = get_unique_key(f'heure_depart_{i}_{j}')
-                    horaire_depart = st.time_input(f"Travel time {villes[f'ville_{i+1}']} to {villes[f'ville_{j+1}']}: ", key=key_1, value = None)
-                    st.write("Travel time :")
+                    horaire_depart = st.time_input(f"Departure time {villes[f'ville_{i+1}']} to {villes[f'ville_{j+1}']}: ", key=key_1, value = None)
+                    st.write("Duration :")
                     col1, col2 = st.columns(2)
                     if horaire_depart is not None:
                         heures_key = get_unique_key(f"heures_{i}_{j}")
@@ -136,7 +240,7 @@ def horaires():
                         minutes = col2.number_input("Minutes:", min_value=0, max_value=59, step=1, key=minutes_key)
                         duree_trajet_minutes = heures * 60 + minutes
                         duree_trajet = f"{heures}:{minutes}"
-                        trajet_key = f"{villes[f'ville_{i+1}']}_{villes[f'ville_{j+1}']}"
+                        trajet_key = f"{villes[f'ville_{i+1}']} > {villes[f'ville_{j+1}']}"
                         horaires_trajets[trajet_key] = {'depart': horaire_depart.strftime("%H:%M"), 'duree': duree_trajet}
                         break
                     else:
@@ -151,6 +255,7 @@ def horaires():
                 try:
                     horaire_depart = st.time_input(f" Departure time {villes[f'ville_{j+1}']} to {villes[f'ville_{i+1}']}: ", key=get_unique_key(f"time_input_{i}{j}"), value = None)
                     col1, col2 = st.columns(2)
+                    st.write("Duration :")
                     if horaire_depart is not None:
                         heures_key = get_unique_key(f"heures_{j}_{i}")
                         minutes_key = get_unique_key(f"minutes_{j}_{i}")
@@ -160,7 +265,7 @@ def horaires():
                             minutes = '0' + str(minutes)
                         if horaire_depart:
                             if duree_trajet:
-                                trajet_key_inverse = f"{villes[f'ville_{j+1}']}_{villes[f'ville_{i+1}']}"
+                                trajet_key_inverse = f"{villes[f'ville_{j+1}']} > {villes[f'ville_{i+1}']}"
                                 horaires_trajets[trajet_key_inverse] = {'depart': horaire_depart, 'duree': duree_trajet}
                                 break
                             else:
@@ -176,11 +281,11 @@ def horaires():
                     st.error("Format d'heure incorrect. Veuillez entrer une heure au format HH:MM.")
 
 
-    return horaires_trajets, num_buses, num_villes, villes
+    return horaires_trajets, num_buses, num_villes, villes, additional_info
 
 def get_days_of_service_all_buses():
 
-    horaires_trajets, num_buses, num_villes, villes = horaires()
+    horaires_trajets, num_buses, num_villes, villes, additional_info = horaires()
     days_of_service_villes = {}
     departs_villes = {}
     arrivees_villes = {}
@@ -255,12 +360,7 @@ def get_days_of_service_all_buses():
                 for i in range(len(arrivees_villes[k])):
                     arrivees_villes[k][i] = departs_villes[k][i] + timedelta(hours=heure_duree, minutes=minutes_duree)
     
-    
-    
-    #fig, table = plot_graphicage_and_table_2_cities(arrivees_villes, departs_villes, villes, horaires_trajets, num_buses, days_of_services)
-    #plot_graphicage_and_table_2_cities(arrivees_villes, departs_villes, villes, horaires_trajets, num_buses, days_of_services)
-    #st.pyplot(fig)
-    #st.pyplot(table)
+
                     
     if st.button("Submit"):
                     
@@ -309,8 +409,8 @@ def get_days_of_service_all_buses():
                         if departs_villes[key] != '':
                             ax.scatter(arrivees_villes[key], [-1]*len(arrivees_villes[key]),  color=colors[key[0]-1])
                             ax.scatter(departs_villes[key], [1]*len(departs_villes[key]),  color=colors[key[0]-1])
-                            ax.plot([arrivees_villes[key], arrivees_villes[key]], [1, -1], linestyle='dotted', color='grey', alpha=0.5)
-                            ax.plot([departs_villes[key], departs_villes[key]], [-1, 1], linestyle='dotted', color='grey', alpha=0.5)
+                            #ax.plot([arrivees_villes[key], arrivees_villes[key]], [1, -1], linestyle='dotted', color='grey', alpha=0.5)
+                            #ax.plot([departs_villes[key], departs_villes[key]], [-1, 1], linestyle='dotted', color='grey', alpha=0.5)
                             plot_arrow(ax, (departs_villes[key][i], 1), (arrivees_villes[key][i], -1), colors[key[0]-1])
                             for i, time in enumerate(departs_villes[key]):
                                 ax.annotate(f"{time.strftime('%H:%M')}",
@@ -321,27 +421,49 @@ def get_days_of_service_all_buses():
                                             (time, -1.07),
                                             textcoords="offset points", xytext=(0, 5), ha='center', fontsize=8)
             
+
             for date in midnight_dates:
-                ax.axvline(x=date, color='lightblue', linestyle='-', linewidth=1)
+                ax.plot([date, date], [-1, 1], color='lightblue', linestyle='-', linewidth=1)
             
-            plt.yticks([-1, 1], [f'{villes["ville_2"]}', f'{villes["ville_1"]}'], fontsize = 20)
-            plt.xticks(dates_noon, [days_of_week[date.weekday()]  for date in dates], fontsize=20, ha='center', va='center')
-            #ax.set_xlim(midnight_dates[0], dates_noon[-1])
-            plt.tick_params(axis='x', which='both', bottom=False, top=True, labelbottom=False, labeltop=True, colors='coral')
+
+            plt.yticks([-1, 1], [ f"{villes['ville_2']}", f"{villes['ville_1']}"], fontsize=20)
+            plt.yticks([-1, 1], [f"{villes['ville_2']}", f"{villes['ville_1']}"], fontsize=20, color = '#054752')
+            plt.xticks(dates_noon, [days_of_week[date.weekday()] for date in dates], fontsize=20, ha='center', va='center', color = '#054752')
+            plt.tick_params(axis='x', which='both', bottom=False, top=True, labelbottom=False, labeltop=True, colors='#054752')
+            plt.gca().xaxis.set_tick_params(pad=-30)
+            x_min, x_max = plt.xlim()
+            y_min, y_max = plt.ylim()
+            rect_x = x_min - 0.05
+            rect_y = y_min - 0.05
+            rect_width = x_max - x_min +0.1
+            rect_height = y_max - y_min +0.1
+
+            # Draw the rectangle around the xtick labels
+            plt.gca().add_patch(plt.Rectangle((rect_x, rect_y), rect_width, rect_height, fill=False, edgecolor='#054752'))
+
             for spine in ax.spines.values():
                 spine.set_visible(False)
 
-            plt.xlim(dates[0], dates[-2])
+            ax.set_xlim(midnight_dates[0], midnight_dates[-2])
+            ax.set_ylim(-2, 1.4)
+            title_color = 'coral'
+            plt.title(f"Graphicage {villes['ville_2']} - {villes['ville_1']}", y=1.08, color=title_color, fontsize=20)
+            title_obj = plt.gca().title
+            title_obj.set_bbox(dict(facecolor='white', edgecolor='coral', boxstyle='round,pad=0.8'))
 
-            title_color = 'navy'
-            #plt.title(f"Graphicage {villes['ville_1']} - {villes['ville_2']}", y=1.1, color=title_color)
-
+            length_unit = 0.15
+            width_unit = 0.03
+            x = 0
+            y = 0
+            bbox = [x,y,length_unit*(len(horaires_trajets.keys())+1),width_unit*(num_buses+1)]
+            table(horaires_trajets, num_buses, days_of_services, colors, bbox)
+            bbox = [x+0.05,y+0.99,1.1,0.1]
+            table_other_info(additional_info, bbox)
             buffer = io.BytesIO()
             plt.savefig(buffer, format='png')
             buffer.seek(0)
             st.image(buffer, use_column_width=True, caption='Graphicage Image')
-            
-            fig.set_size_inches(18, 10)
+            fig.set_size_inches(20*0.5, 15*0.5)
             plt.subplots_adjust(left=0.15, right=0.9, top=0.9, bottom=0.3)
             plt.savefig(f"Graphicage {villes['ville_1']} - {villes['ville_2']}.png", bbox_inches='tight')
             plt.close()
@@ -378,7 +500,7 @@ def get_days_of_service_all_buses():
                 ville_3_2 = list(departs_villes.keys())[5][2]
             else:
                 ville_3_2 = 'not a key'
-
+            
             # Afficher la première valeur des clés contenant cet élément
             for key, value in departs_villes.items():
                 if ville_3_2 in key:
@@ -500,42 +622,62 @@ def get_days_of_service_all_buses():
                                 (time, -0.03),
                                 textcoords="offset points", xytext=(0, -20), ha='center', fontsize=8)
 
-                
-            plt.xticks(dates_noon, [days_of_week[date.weekday()]  for date in dates], fontsize = 20, ha='center', va='center')
-            ax.set_xlim(midnight_dates[0], dates_noon[-1])
-            plt.tick_params(axis='x', which='both', bottom=False, top=True, labelbottom=False, labeltop=True, colors = 'coral')
-            #plt.title(f"Graphicage {villes['ville_1']} - {villes['ville_2']} - {villes['ville_3']}" , y=1.1)  # Déplacer le titre plus haut
-            for spine in ax.spines.values():
-                spine.set_visible(False)  # Désactiver les bords du graphique
-            plt.yticks([-1, 0, 1], [f"{villes['ville_3']}", f"{villes['ville_2']}", f"{villes['ville_1']}"], fontsize=20)
-            days_to_next_monday = (7 - start_date.weekday()) % 7  # Calculate days to the next Monday
-            start_date = datetime.now() + timedelta(days=(7 - datetime.now().weekday()))
-            dates = [start_date + timedelta(days=i) for i in range(8)]  # Générer jusqu'au jour 8 inclus
+
             for date in midnight_dates:
-                ax.axvline(x=date, color='lightblue', linestyle='-', linewidth=1)  # Modifier la couleur et le style selon vos préférences
-            plt.xlim(dates[0], dates[-2])
-            title_color = 'navy'
+                ax.plot([date, date], [-1, 1], color='lightblue', linestyle='-', linewidth=1)
+            
+            plt.yticks([-1, 0, 1], [f"{villes['ville_3']}", f"{villes['ville_2']}", f"{villes['ville_1']}"], fontsize=20, color = '#054752')
+            
+            plt.xticks(dates_noon, [days_of_week[date.weekday()] for date in dates], fontsize=20, ha='center', va='center', color = '#054752')
+            plt.tick_params(axis='x', which='both', bottom=False, top=True, labelbottom=False, labeltop=True, colors='#054752')
+            plt.gca().xaxis.set_tick_params(pad=-30)
+            x_min, x_max = plt.xlim()
+            y_min, y_max = plt.ylim()
+            rect_x = x_min - 0.05
+            rect_y = y_min - 0.05
+            rect_width = x_max - x_min +0.1
+            rect_height = y_max - y_min +0.1
+
+            # Draw the rectangle around the xtick labels
+            plt.gca().add_patch(plt.Rectangle((rect_x, rect_y), rect_width, rect_height, fill=False, edgecolor='#054752'))
+
+            for spine in ax.spines.values():
+                spine.set_visible(False)
+
+            ax.set_xlim(midnight_dates[0], midnight_dates[-2])
+            ax.set_ylim(-2, 1.6)
+            title_color = 'coral'
+            plt.title(f"Graphicage {villes['ville_3']} - {villes['ville_2']} - {villes['ville_1']}", y=1.08, color=title_color, fontsize=20)
+            title_obj = plt.gca().title
+            title_obj.set_bbox(dict(facecolor='white', edgecolor='coral', boxstyle='round,pad=0.8'))
+
+            length_unit = 0.15
+            width_unit = 0.03
+            x = 0
+            y = 0
+            bbox = [x,y,length_unit*(len(horaires_trajets.keys())+1),width_unit*(num_buses+1)]
+            table(horaires_trajets, num_buses, days_of_services, colors, bbox)
+            bbox = [x+0.05,y+0.99,1,0.1]
+            table_other_info(additional_info, bbox)
             buffer = io.BytesIO()
             plt.savefig(buffer, format='png')
             buffer.seek(0)
             st.image(buffer, use_column_width=True, caption='Graphicage Image')
-            
-
-            fig.set_size_inches(18*0.9, 10*0.9)
+            fig.set_size_inches(20*0.5, 15*0.5)
             plt.subplots_adjust(left=0.15, right=0.9, top=0.9, bottom=0.3)
-            plt.savefig(f"Graphicage {villes['ville_1']} - {villes['ville_2']}.png", bbox_inches='tight')
+            plt.savefig(f"Graphicage {villes['ville_1']} - {villes['ville_2']} -  {villes['ville_3']}.png", bbox_inches='tight')
             plt.close()
 
-            filename = f"Graphicage {villes['ville_1']} - {villes['ville_2']} - {villes['ville_3']}.png"  # Define the filename for the graph image
+            filename = f"Graphicage {villes['ville_1']} - {villes['ville_2']} -  {villes['ville_3']}.png"  # Define the filename for the graph image
             image_bytes = buffer.getvalue()
             st.markdown(get_image_download_link(image_bytes, filename=filename), unsafe_allow_html=True)
 
-
-
-
+            
 
     
 
 if __name__ == '__main__':
     get_days_of_service_all_buses()
+
+    
 
